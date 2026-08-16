9ab940fd4da8bcec4be984ba20835146b54f4e67
aayush <aayushd711@gmail.com>
2026-08-16 13:49:10 +0530

    iam polict added ssm

--- terraform/gitlab-runner/iam-policy.tf
+++ terraform/gitlab-runner/iam-policy.tf
@@ -3,3 +3,8 @@ resource "aws_iam_role_policy_attachment" "ecr-power-user" {
   role       = aws_iam_role.iam-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
+
+resource "aws_iam_role_policy_attachment" "ssm-attach" {
+  role       = aws_iam_role.iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
